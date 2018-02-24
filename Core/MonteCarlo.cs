06594a3004d1641c54249b34d1ada0664bc3eafd
Dominik Hyll <dominik.hyll@m2ms.sk>
2018-02-24 17:15:08 +0100

    Add project files.

---
+++ Core/MonteCarlo.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class MonteCarlo
+    {
+
+        public int DoorsCount { get; set; }
+        public long Replications { get; set; }
+        public int FirstChoiseWinning { get; set; } = 0;
+        public int ChangedChoiseWinning { get; set; } = 0;
+
+        public delegate void NewResult(long replications, decimal p);
+
+        public NewResult FirstChoiceResult;
+        public NewResult ChangedChoiceResult;
+
+
+        public MonteCarlo(int doorsCount, long replications = 10000000)
+        {
+            this.DoorsCount = doorsCount;
+            this.Replications = replications;
+        }
+
+        public Task RunExperiment()
+        {
+            return Task.Run(() =>
+            {
+                var every = Replications >= 1000 ? Replications /100 : Replications /10;
+
+                var seedRandom = new Random();
+
+                var winningDoorGenerator = new Random(seedRandom.Next());
+                var userChoiseGenerator = new Random(seedRandom.Next());
+                var doorToOpenGenerator = new Random(seedRandom.Next());
+
+                int winningChoise, userFirstChoise, userChangedChoise, openedDoors;
+
+                for (long i = 1; i <= Replications; i++)
+                {
+                    winningChoise = winningDoorGenerator.Next(DoorsCount);
+                    
+                    userFirstChoise = userChoiseGenerator.Next(DoorsCount);
+
+                    do
+                    {
+                        openedDoors = doorToOpenGenerator.Next(DoorsCount);
+                    } while (openedDoors == winningChoise || openedDoors == userFirstChoise);
+
+                    do
+                    {
+                        userChangedChoise = userChoiseGenerator.Next(DoorsCount);
+                    } while (userChangedChoise == userFirstChoise || userChangedChoise == openedDoors);
+
+                    if (userFirstChoise == winningChoise) FirstChoiseWinning++;
+                    else if (userChangedChoise == winningChoise) ChangedChoiseWinning++;
+
+                    if (i % (every) == 1 || i == Replications)
+                    {
+                        FirstChoiceResult?.Invoke(i, ((decimal)FirstChoiseWinning / i) * 100);
+                        ChangedChoiceResult?.Invoke(i, ((decimal)ChangedChoiseWinning / i) * 100);
+                    }
+                }
+            });
+
+        }
+
+    }
+}
---
+++ GUI/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using Core;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public SeriesCollection SeriesCollection { get; set; }
+        public LineSeries LineSeriesFirstChoice { get; set; }
+        public LineSeries LineSeriesChangedChoice { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            LineSeriesFirstChoice = new LineSeries()
+            {
+                Title = "First choice winning rate",
+                Fill = Brushes.Transparent,
+                PointGeometrySize = 5,
+            };
+            LineSeriesChangedChoice = new LineSeries()
+            {
+                Title = "Changed choice winning rate",
+                Fill = Brushes.Transparent,
+                PointGeometrySize = 5,
+            };
+
+            SeriesCollection = new SeriesCollection()
+            {
+
+                LineSeriesChangedChoice,
+                LineSeriesFirstChoice,
+
+            };
+        }
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Int32.TryParse(DoorCount.Text, out var doors) || (doors < 3 || doors > 10))
+            {
+                MessageBox.Show("Please insert correct number of doors <3,10>", "Wrong input");
+                return;
+            }
+            if (!Int64.TryParse(Replications.Text, out var replications) || replications < 100)
+            {
+                MessageBox.Show("Please insert correct number of replications (> 100)", "Wrong input");
+                return;
+            }
+
+            RunButton.IsEnabled = false;
+            var mc = new MonteCarlo(doors, replications);
+            var percentage = 0;
+
+            LineSeriesFirstChoice.Values = new ChartValues<ObservablePoint>();
+            LineSeriesChangedChoice.Values = new ChartValues<ObservablePoint>();
+            mc.ChangedChoiceResult += (i, p) =>
+           {
+               Application.Current.Dispatcher.InvokeAsync(() =>
+               {
+                   LineSeriesChangedChoice.Values.Add(new ObservablePoint(i, (double)p));
+                   Ccl.Content = $"Changed choice winning rate: {p: 0.00}%";
+                   Progress.Content = $"{++percentage} % Done";
+               });
+
+           };
+            mc.FirstChoiceResult += (i, p) =>
+            {
+                Application.Current.Dispatcher.InvokeAsync(() =>
+                {
+                    LineSeriesFirstChoice.Values.Add(new ObservablePoint(i, (double)p));
+                    Fcl.Content = $"First choice winning rate: {p: 0.00}%";
+                });
+
+            };
+
+            await mc.RunExperiment();
+            RunButton.IsEnabled = true;
+            await Application.Current.Dispatcher.InvokeAsync(() =>
+             {
+                 Progress.Content = $"100 % Done";
+             });
+
+        }
+
+    }
+}
